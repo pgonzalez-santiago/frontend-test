5225c29501466dc10b7ba6505a305d1af10176f2
Pedro González-Santiago González <perico.gonzalezsantiago@gmail.com>
2018-11-29 18:17:09 +0100

    Remove duplicated comment

--- src/containers/contributorsList.js
+++ src/containers/contributorsList.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 // Redux
-// Redux
 import RepositoryContributorsActions from '../store/reducers/repositoryContributors'
 
 // Components
